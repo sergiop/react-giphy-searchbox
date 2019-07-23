022a38fc6b948c63b8e9acd5d6961a55527023f6
Sergio Pedercini <sergio.pedercini@gmail.com>
2019-07-24 01:08:02 +0200

    Improvements on demo page.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,30 @@
+// @flow
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import ReactGiphySearchAndSelect from '../../src/index'
+import ReactGiphySearchBox from '../../src'
+import styles from './index.css'
 
 class Demo extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.wrapper}>
         <h1>react-giphy-searchbox Demo</h1>
-        <ReactGiphySearchAndSelect />
+        {/* <ReactGiphySearchBox
+          masonryConfig={[
+            { columns: 2, imageWidth: 140, gutter: 10 },
+            { mq: '700px', columns: 3, imageWidth: 200, gutter: 10 },
+            { mq: '1000px', columns: 4, imageWidth: 220, gutter: 10 },
+          ]}
+        /> */}
+        <div className={styles.searchBox}>
+          <ReactGiphySearchBox
+            apiKey="9Ixlv3DWC1biJRI57RanyL7RTbfzz0o7"
+            // wrapperClass="pippo"
+            // wrapperStyle={{ backgroundColor: 'red' }}
+          />
+        </div>
       </div>
     )
   }
