8e758b95c28d89405122dcd766dfd5d92cc37d83
Sergio Pedercini <sergio.pedercini@gmail.com>
2019-07-26 16:48:03 +0200

    Remove commented example.

--- demo/src/index.js
+++ demo/src/index.js
@@ -10,13 +10,6 @@ const Demo = () => (
   <div className={styles.wrapper}>
     <h1>react-giphy-searchbox Demo</h1>
     <div className={styles.searchBox}>
-      {/* <ReactGiphySearchBox
-        masonryConfig={[
-          { columns: 2, imageWidth: 140, gutter: 10 },
-          { mq: '700px', columns: 3, imageWidth: 200, gutter: 10 },
-          { mq: '1000px', columns: 4, imageWidth: 220, gutter: 10 },
-        ]}
-      /> */}
       <ReactGiphySearchBox
         apiKey="9Ixlv3DWC1biJRI57RanyL7RTbfzz0o7"
         // eslint-disable-next-line no-console
