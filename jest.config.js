cd332e29c235dd0bbef02bdbdcb27d769ff01bfe
Sergio Pedercini <sergio.pedercini@gmail.com>
2019-07-31 14:27:34 +0200

    Add support for CssModules and static assets.

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,10 @@ module.exports = {
       statements: 80,
     },
   },
-  setupFiles: ['./setupTests'],
   setupFilesAfterEnv: ['./react-testing-library.setup.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.css$': 'identity-obj-proxy',
+  },
 }
