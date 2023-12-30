3f8302ea7faa4590251d70d345f4393a538a1ceb
Sergio Pedercini <sergio.pedercini@gmail.com>
2023-12-30 17:56:41 +0100

    Add airbnb eslint and prettier.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,31 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "prettier",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', "prettier"],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    "prettier/prettier": [
+      "error", {
+        "singleQuote": true,
+        "trailingComma": "all"
+      }
+    ],
+    "react/require-default-props": ["off"],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
