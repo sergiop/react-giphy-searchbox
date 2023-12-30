53bde2fcb6fa2511eabcd5b51f88a09cb87cda28
Sergio Pedercini <sergio.pedercini@gmail.com>
2023-12-30 18:02:34 +0100

    Improve config and format file.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,33 +2,35 @@ module.exports = {
   root: true,
   env: { browser: true, es2021: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "airbnb",
     "airbnb-typescript",
     "airbnb/hooks",
     "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
   },
-  plugins: ['react-refresh', "prettier"],
+  plugins: ["react-refresh", "prettier"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
+    "react/react-in-jsx-scope": "off",
     "prettier/prettier": [
-      "error", {
-        "singleQuote": true,
-        "trailingComma": "all"
-      }
+      "error",
+      {
+        singleQuote: true,
+        trailingComma: "all",
+      },
     ],
-    "react/require-default-props": ["off"],
-    'react-refresh/only-export-components': [
-      'warn',
+    "import/prefer-default-export": "off",
+    "react/require-default-props": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
