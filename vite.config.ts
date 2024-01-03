8bb25196a640932e9397907a28258e558b493bff
Sergio Pedercini <sergio.pedercini@gmail.com>
2024-01-03 01:13:20 +0100

    Import defineConfig from vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
